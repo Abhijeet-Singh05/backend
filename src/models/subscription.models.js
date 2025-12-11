3b551806b572c7f5877c054153dc27c4d91d6617
Abhijeet-Singh05 <singh.abhijeet9342@gmail.com>
2025-12-11 21:26:36 +0530

    add subscription model

---
+++ src/models/subscription.models.js
@@ -0,0 +1,18 @@
+import mongoose, {Schema}  from "mongoose";
+
+const subscriptionSchema = new Schema({
+
+    subscriber: {
+        type: Schema.Types.ObjectId, // one who subscribing the Channel(User)
+        ref: "User"
+    },
+    channel: {
+        type: Schema.Types.ObjectId, // one to whom subsciber is subscribing
+        ref: "User"
+    }
+    
+
+
+},{timestamps:true});
+
+export const Subscription = mongoose.model("Subscription",subscriptionSchema);
